3ccd2bf306a7dabfa8ec3cf89f0a8015459f40b2
archima20 <shima.naderii@gmail.com>
2023-08-15 11:58:49 +0200

    changes

--- regularexpression.py
+++ regularexpression.py
@@ -2,5 +2,13 @@ import re
 
 result = re.search(r"aza", "plaza")
 print(result)
-
 # <re.Match object; span=(2, 5), match='aza'> is what printed 
+
+result= re.search(r"aza", "bazaar")
+print(result)
+# <re.Match object; span=(2, 5), match='aza'>
+# <re.Match object; span=(1, 4), match='aza'>
+
+result= re.search(r"aza", "bammar")
+print(result)
+# None
