a082162c2b889fffccc2914cd0951a50e5a7fd4e
archima20 <shima.naderii@gmail.com>
2023-08-22 23:16:08 +0200

    add changes

--- regularexpression.py
+++ regularexpression.py
@@ -82,3 +82,10 @@ import re
 def repeating_letter_a(text):
   result = re.search(r"___", text)
   return result != None
+
+print(re.search(r".com", "welcome"))
+# <re.Match object; span=(2, 6), match='lcom'>
+
+print(re.search(r"\.com", "welcome"))
+#None
+
