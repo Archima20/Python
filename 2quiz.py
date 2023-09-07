b86e293f726c42fa5dcc93e1449aea6b65e5250b
archima20 <shima.naderii@gmail.com>
2023-09-07 18:10:26 +0200

    add a quiz file

--- 2quiz.py
+++ 2quiz.py
@@ -1,3 +1,5 @@
+
+# 1.The check_web_address function checks if the text passed qualifies as a top-level web address, meaning that it contains alphanumeric characters (which includes letters, numbers, and underscores), as well as periods, dashes, and a plus sign, followed by a period and a character-only top-level domain such as ".com", ".info", ".edu", etc. Fill in the regular expression to do that, using escape characters, wildcards, repetition qualifiers, beginning and end-of-line characters, and character classes.
 import re
 def check_web_address(text):
     pattern = r"^[a-zA-Z0-9._+-]+\.[a-zA-Z]+$"
@@ -9,3 +11,16 @@ print(check_web_address("www@google"))  # False
 print(check_web_address("www.Coursera.org"))  # True
 print(check_web_address("web-address.com/homepage"))  # False
 print(check_web_address("My_Favorite-Blog.US"))  # True
+
+
+# 2.The check_time function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with no leading zero, followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case. Fill in the regular expression to do that. How many of the concepts that you just learned can you use here?
+import re
+def check_time(text):
+  pattern = r'^([0-9]|1[0-2]):[0-5][0-9]\s?(AM|am|PM|pm)$'
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
