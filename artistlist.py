fd3e9be907fd61c710a6aee65205cd8c9a8405b4
archima20 <shima.naderii@gmail.com>
2023-09-08 11:03:42 +0200

    CodeBar Practice

---
+++ artistlist.py
@@ -0,0 +1,19 @@
+apikey = "5b97c38c59e3edf289c47d714095291a"
+url = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="
+artist = "miles+davis"
+url+=artist
+url+="&api_key=5b97c38c59e3edf289c47d714095291a&format=json"
+print(url)
+print(f"https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist={artist}&api_key={apikey}&format=json")
+
+import urllib.request
+with urllib.request.urlopen(url) as response:
+    rawdata = response.read()
+
+import json
+data = json.loads(rawdata)
+print(data["similarartists"]["artist"][1]["name"])
+
+artists = data["similarartists"]["artist"]
+for i, artist in enumerate(artists):
+    print(f"{i+1}: {artist['name']} - {artist['match']}")
