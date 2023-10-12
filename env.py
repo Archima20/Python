179e03341656baec05c57003683367f7ddda9d71
archima20 <shima.naderii@gmail.com>
2023-10-12 21:32:13 +0200

    subprocess

--- env.py
+++ env.py
@@ -1,15 +1,17 @@
-# cat variables.py 
-# cat google.py 
+
 
 # import os
-import subprocess
-subprocess.run(["cmd", "/c", "date"])
-
-# subprocess.run(["date"]) is a linux command!
-
-
-
 # print("HOME: " + os.environ.get("HOME", ""))
 # print("SHELL: "+ os.environ.get("SHELL", ""))
 # print("Fruit: " + os.environ.get("Fruit", ""))
 # export fruit=Pineapple
+
+import subprocess
+subprocess.run(["cmd", "/c", "date"])
+
+# subprocess.run(["date"]) is a linux command! 
+# In windows, we are using the cmd shell to execute the date command. The /c flag is used to run the command and then terminate the cmd process.
+
+
+
+
