15bc7452bc8c10711ed8759099fce0998364def8
archima20 <shima.naderii@gmail.com>
2023-10-12 21:33:39 +0200

    subprocess

--- env.py
+++ env.py
@@ -12,6 +12,11 @@ subprocess.run(["cmd", "/c", "date"])
 # subprocess.run(["date"]) is a linux command! 
 # In windows, we are using the cmd shell to execute the date command. The /c flag is used to run the command and then terminate the cmd process.
 
+from datetime import datetime
 
+current_date_time = datetime.now()
+print(current_date_time)
+
+# This Python code will give you the current date and time.
 
 
