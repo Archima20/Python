ea7e8b70a0221a26539cb3654c7c635db9313e94
archima20 <shima.naderii@gmail.com>
2023-10-17 16:17:37 +0200

    failed test

--- erarrange_test.py
+++ erarrange_test.py
@@ -1,13 +1,11 @@
-from rearrange import rearrange_name 
 import unittest
+from rearrange import rearrange_name  # Ensure to import the rearrange_name function from the correct module
 
 class TestRearrange(unittest.TestCase):
     def test_basic(self):
         testcase = "Lovelace, Ada"
         expected = "Ada Lovelace"
         self.assertEqual(rearrange_name(testcase), expected)
-        
-#if __name__ == '__main__':  #added by chatGPT :block to ensure that the test suite is executed only when the script is run directly, not when imported as a module.
-     #unittest.main()
-     
-unittest.main()
+
+if __name__ == '__main__':
+    unittest.main()
