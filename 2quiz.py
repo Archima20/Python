64d54c979227d1fa07bae25e85c01786a6322c04
Shima Naderi <shima.naderii@gmail.com>
2023-11-29 12:43:29 +0100

    Update 2quiz.py

--- 2quiz.py
+++ 2quiz.py
@@ -7,8 +7,8 @@ def check_web_address(text):
     result = re.search(pattern, text)
     return result is not None
 
-print(check_web_address("gmail.com"))  # True
-print(check_web_address("www@google"))  # False
+print(check_web_address("gmail.com"))  # True!
+print(check_web_address("www@google"))  # False!
 print(check_web_address("www.Coursera.org"))  # True
 print(check_web_address("web-address.com/homepage"))  # False
 print(check_web_address("My_Favorite-Blog.US"))  # True
