caeab58d47ae1a4f6f850c07654ea60689bea5fd
Shima Naderi <shima.naderii@gmail.com>
2023-12-01 13:15:35 +0100

    Update 2quiz.py

--- 2quiz.py
+++ 2quiz.py
@@ -11,7 +11,7 @@ print(check_web_address("gmail.com"))  # True!
 print(check_web_address("www@google"))  # False
 print(check_web_address("www.Coursera.org"))  # True!
 print(check_web_address("web-address.com/homepage"))  # False
-print(check_web_address("My_Favorite-Blog.US"))  # True
+print(check_web_address("My_Favorite-Blog.US"))  # True!
 
 
 # 2.The check_time function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with no leading zero, followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case. Fill in the regular expression to do that. How many of the concepts that you just learned can you use here?
