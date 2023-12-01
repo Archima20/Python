2c3c306b5c2c07deec27efa94970271e2ccd83c7
Shima Naderi <shima.naderii@gmail.com>
2023-12-01 13:15:23 +0100

    Update 2quiz.py

--- 2quiz.py
+++ 2quiz.py
@@ -1,4 +1,4 @@
-# Module 2 week 3 I passed with Chat GPT`s help! as always :))
+# Module 2 week 3 
 # Practice Quiz: Basic Regular Expressions.
 # 1.The check_web_address function checks if the text passed qualifies as a top-level web address, meaning that it contains alphanumeric characters (which includes letters, numbers, and underscores), as well as periods, dashes, and a plus sign, followed by a period and a character-only top-level domain such as ".com", ".info", ".edu", etc. Fill in the regular expression to do that, using escape characters, wildcards, repetition qualifiers, beginning and end-of-line characters, and character classes.
 import re
