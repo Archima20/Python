50042a5d9b4e7b01ed1e424f1ecc37e1c124ab3a
Shima Naderi <shima.naderii@gmail.com>
2024-01-09 14:56:16 +0100

    Update CreatSql.sql

--- CreatSql.sql
+++ CreatSql.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
 -- SQL Commands to set up the pmadb as described in the documentation.
 --
--- This file is meant for use with MySQL 5 and above!!!
+-- This file is meant for use with MySQL 5 and above!
 --
 -- This script expects the user pma to already be existing. If we would put a
 -- line here to create them too many users might just use this script and end
